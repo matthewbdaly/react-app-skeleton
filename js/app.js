c926ac966c6db622fcd61f3d4f2febba1e31111b
Matthew Daly <matthewbdaly@gmail.com>
2015-11-08 13:39:43 +0000

    Added JS

---
+++ js/app.js
@@ -0,0 +1,11 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Router = require('react-router').Router
+var Route = require('react-router').Route
+var Link = require('react-router').Link
+var Page = require('./components/page.js');
+
+ReactDOM.render(
+  <Page />,
+  document.getElementById('view')
+);
---
+++ js/components/page.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var Page = React.createClass({
+  render: function () {
+    return (
+      <div className="page">
+        <h1>My Page</h1>
+      </div>
+    )
+  }
+});
+
+module.exports = Page;
