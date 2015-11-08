45eaa1b0d0d4346b9c640e388ae3439babaa724e
Matthew Daly <matthewbdaly@gmail.com>
2015-11-08 13:31:20 +0000

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var webpack = require('webpack');  
+module.exports = {  
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./js/app.js",
+      "./css/main.scss"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+            { test: /\.scss$/, loader: "style!css!sass" }
+        ]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+
+};
