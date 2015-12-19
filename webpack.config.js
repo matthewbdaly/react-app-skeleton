a2a965f10464882f37536e679b59dbab20a47ab5
Matthew Daly <matthewbdaly@gmail.com>
2015-12-19 11:48:52 +0000

    Now lint JS

--- webpack.config.js
+++ webpack.config.js
@@ -10,11 +10,21 @@ module.exports = {
         filename: "bundle.js"
     },
     module: {
+        preLoaders: [
+          {
+            test: /(\.js$|\.jsx$)/, 
+            exclude: /node_modules/, 
+            loader: "eslint-loader"
+          }
+        ],
         loaders: [
             { test: /\.js?$/, loaders: ['react-hot', 'babel'], exclude: /node_modules/ },
             { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
             { test: /\.scss$/, loader: "style!css!sass" }
         ]
+    },
+    eslint: {
+      configFile: '.eslintrc.yml'
     },
     plugins: [
       new webpack.NoErrorsPlugin()
